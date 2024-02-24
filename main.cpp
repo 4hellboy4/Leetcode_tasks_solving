49ea67558f7ecdb10ff915d6f4be05c40bfa3dfb
4hellboy4 <Fred19811@yandex.ru>
2024-02-24 13:53:35 +0300

    first alhpabet almost works

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <algorithm>
+
 using namespace std;
 
 //vector<str> solution1() {
@@ -11,17 +13,44 @@ using namespace std;
 
 set<string> my_set;
 
-void permutations(int& n, vector<string>& array, string str) {
+int count_symbol(string& str, char& c) {
+    int cnt = 0;
+    for (int i = 0; i < str.size(); ++i) {
+        cnt += (str.at(i) == c ? 1 : 0);
+    }
+    return cnt;
+}
+
+void alphabet1(vector<char>& symbols, int& num) {
+    vector<string> temp;
+    for (string word : my_set) {
+        bool flag = true;
+        for (char s : symbols) {
+            int cnt = count_symbol(word, s);
+            if (cnt % 2 != 0) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+            temp.emplace_back(word);
+    }
+
+    sort(temp.begin(), temp.end());
+
+    for (int i = 0; i < num - 1; ++i) {
+        cout << temp[i] << " ";
+    }
+}
+
+
+
+void permutations(int& n, vector<char>& array, string str) {
     if ((str.size() > 2*n) || my_set.find(str) != my_set.end())
         return;
-    array.emplace_back(str);
     my_set.insert(str);
-    for (int i = 0; i < array.size(); ++i) {
-        string temp = array.at(i);
-        for (string& s : array) {
-            temp.append(s);
-            permutations(n, array, temp);
-        }
+    for (char s : array) {
+        permutations(n, array, str + s);
     }
 }
 
@@ -31,7 +60,7 @@ int main() {
 
     cin >> n;
 
-    vector<string> symbols(n);
+    vector<char> symbols(n);
     vector<int> lengths(3);
 
     for (int i = 0; i < n; ++i) {
@@ -42,18 +71,15 @@ int main() {
         cin >> lengths.at(i);
     }
 
-//    vector<string> strings;
-    vector<string> strings;
-    for (string s : symbols) {
-        strings.emplace_back(s);
-        my_set.insert(s);
+    for (char s : symbols) {
+        permutations(n, symbols, "");
     }
 
-    permutations(n, strings, "");
+//    for (string word : my_set) {
+//        cout << word << endl;
+//    }
 
-    for (string s : strings) {
-        cout << s << endl;
-    }
-
+    cout << "_" << " ";
+    alphabet1(symbols, lengths[0]);
 
 }
