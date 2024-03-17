dfde34ec93d4036400bb42cd923e5ab2d65511f9
4hellboy4 <Fred19811@yandex.ru>
2024-03-17 17:52:33 +0300

    443 String Compression

--- main.cpp
+++ main.cpp
@@ -1,80 +1,69 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-class Pair {
-public:
-    int element;
-    int count;
+string divide(long long num) {
+    string temp;
+    while (num != 0) {
+        temp += to_string(num%10);
+        num /= 10;
+    }
+    reverse(temp.begin(), temp.end());
+    return temp;
+}
 
-    Pair(int element, int count) : element(element), count(count) {}
-};
+int compress(vector<char>& chars) {
+    string s = "";
+    if (!chars.empty()) {
+        char current = chars[0];
+        int cnt = 1;
+        for (int i = 1; i < chars.size(); ++i) {
+            if (current == chars[i]) {
+                cnt += 1;
+            } else {
+                if (cnt == 1) {
+                    s += current;
+                } else {
+                    s += current;
+                    s += divide(cnt);
+                }
+                current = chars[i];
+                cnt = 1;
+            }
+        }
+        if (cnt == 1) {
+            s += current;
+        } else {
+            s += current;
+            s += divide(cnt);
+        }
+    }
+    chars.clear();
+    for (char sym : s) {
+        chars.emplace_back(sym);
+    }
+    cout << s << endl;
+
+    return (int) s.size();
+}
 
 int main() {
     int n;
     cin >> n;
 
-    vector<int> nums(n);
+    vector<char> chars(n);
 
     for (int i = 0; i < n; ++i) {
-        cin >> nums[i];
+        cin >> chars[i];
     }
 
-    vector<Pair> arr;
+    cout << compress(chars) << endl;
 
-    int current_num = nums[0];
-    int cnt = 1;
-    int i = 1;
-
-    while (i < nums.size()) {
-        int current = nums[i];
-        if (current_num != current) {
-            Pair temp = Pair(current_num, cnt);
-            arr.emplace_back(temp);
-
-            current_num = nums[i];
-            cnt = 1;
-        } else {
-            cnt += 1;
-        }
-        i++;
+    for (char sym : chars) {
+        cout << sym << " ";
     }
-
-    Pair temp = Pair(current_num, cnt);
-    arr.emplace_back(temp);
-
-    int answer = max(arr[0].element == 1 ? (arr[0].count) : 0,
-                     arr[arr.size() - 1].element == 1
-                     ? (arr[arr.size() - 1].count)
-                     : -1);
-
-    if (arr.size() == 1 && arr[0].element == 0) {
-        answer = 0;
-    } else if (arr.size() == 1 && arr[0].element == 1) {
-        answer = answer - 1;
-    }
-
-
-    i = 0;
-
-    while (i < arr.size() - 1) {
-        if (arr[i].element == 1) {
-            answer = max(answer, arr[i].count);
-            if ( i + 2 < arr.size()) {
-                answer = max(answer,
-                             (arr[i + 1].count == 1 && arr[i + 2].element == 1)
-                             ? (arr[i].count + arr[i + 2].count)
-                             : 0);
-            }
-            answer = max(answer, arr[i + 1].element == 1
-                                 ? arr[i].count + arr[i + 1].count - 1
-                                 : 0);
-        }
-        i++;
-    }
-
-    cout << answer;
 
     return 0;
 }
